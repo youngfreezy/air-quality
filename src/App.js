4921b718d6daef3453d3adcbff21518abbdebec7
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2021-05-25 13:18:12 -0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 
-import _ from "lodash";
-
 import React, {useEffect, useRef, useReducer, useCallback} from "react";
 import {Search, Grid} from "semantic-ui-react";
 
@@ -10,6 +8,7 @@ const initialState = {
   loading: false,
   results: [],
   value: "",
+  valueSelection: null,
 };
 
 function exampleReducer(state, action) {
@@ -21,7 +20,7 @@ function exampleReducer(state, action) {
     case "FINISH_SEARCH":
       return {...state, loading: false, results: action.results};
     case "UPDATE_SELECTION":
-      return {...state, value: action.selection};
+      return {...state, value: action.selection, valueSelection: action.selection};
 
     default:
       throw new Error();
@@ -30,7 +29,7 @@ function exampleReducer(state, action) {
 
 function SearchExampleStandard() {
   const [state, dispatch] = useReducer(exampleReducer, initialState);
-  const {loading, results, value} = state;
+  const {loading, results, value, valueSelection} = state;
 
   const timeoutRef = useRef();
   const handleSearchChange = useCallback((e, data) => {
@@ -44,14 +43,20 @@ function SearchExampleStandard() {
       }
 
       const citySearch = data.value;
+      // get the latest ten measurements
       const searchResults = fetch(
-        `https://docs.openaq.org/v2/cities?limit=100&page=1&offset=0&sort=asc&city=${citySearch}&order_by=city`
+        `https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements?date_from=2000-01-01T00%3A00%3A00%2B00%3A00&date_to=2021-05-25T19%3A40%3A00%2B00%3A00&limit=10&page=1&offset=0&sort=desc&unit=ppm&radius=1000&city=${citySearch}&order_by=datetime&isAnalysis=true`
       )
         .then((response) => response.json())
         .then((data) =>
           dispatch({
             type: "FINISH_SEARCH",
-            results: data.results,
+            results: data.results.map((item) => {
+              return {
+                title: item.city,
+                description: `${item.value} ${item.unit}`,
+              };
+            }),
           })
         )
         .catch((err) => {
@@ -66,28 +71,36 @@ function SearchExampleStandard() {
   }, []);
 
   return (
-    <Search
-      loading={loading}
-      onResultSelect={(e, data) =>
-        dispatch({type: "UPDATE_SELECTION", selection: data.result.title})
-      }
-      onSearchChange={handleSearchChange}
-      results={results}
-      value={value}
-    />
+    <div>
+      {/* <pre style={{overflowX: "auto"}}>{JSON.stringify({loading, results, value}, null, 2)}</pre> */}
+      <Search
+        loading={loading}
+        onResultSelect={(e, data) =>
+          dispatch({type: "UPDATE_SELECTION", selection: data.result.title})
+        }
+        onSearchChange={handleSearchChange}
+        results={results}
+        value={value}
+      />
+      {valueSelection && results?.length > 0 && (
+        <span>
+          Air Quality for {valueSelection}: {results[0].description}{" "}
+        </span>
+      )}
+    </div>
   );
 }
 
 function App() {
   return (
     <Grid columns={2} className={"search-row"}>
-      <span> Compare Air Quality in Two Cities in the USA</span>
+      <span> Compare Air Quality in Two Cities in the USA (case sensitive)</span>
       <Grid.Row>
         <Grid.Column>
-          <SearchExampleStandard></SearchExampleStandard>
+          <SearchExampleStandard key={1}></SearchExampleStandard>
         </Grid.Column>
         <Grid.Column>
-          <SearchExampleStandard></SearchExampleStandard>
+          <SearchExampleStandard key={2}></SearchExampleStandard>
         </Grid.Column>
       </Grid.Row>
     </Grid>
