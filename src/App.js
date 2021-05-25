941a2f080322a486a115d0812e6e9b5c3b13cff8
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2021-05-25 11:28:13 -0700

    save work so far

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+
+import _ from "lodash";
+import faker from "faker";
+import React, {useEffect} from "react";
+import {Search, Grid, Header, Segment} from "semantic-ui-react";
+
+// swap out source/mock data below with this api call
+// https://docs.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&radius=1000&country=US&city=Chicago&order_by=lastUpdated&dumpRaw=false
+
+const source = _.times(5, () => ({
+  title: faker.company.companyName(),
+  description: faker.company.catchPhrase(),
+  image: faker.internet.avatar(),
+  price: faker.finance.amount(0, 100, 2, "$"),
+}));
+
+const initialState = {
+  loading: false,
+  results: [],
+  value: "",
+};
+
+function exampleReducer(state, action) {
+  switch (action.type) {
+    case "CLEAN_QUERY":
+      return initialState;
+    case "START_SEARCH":
+      return {...state, loading: true, value: action.query};
+    case "FINISH_SEARCH":
+      return {...state, loading: false, results: action.results};
+    case "UPDATE_SELECTION":
+      return {...state, value: action.selection};
+
+    default:
+      throw new Error();
+  }
+}
+
+function SearchExampleStandard() {
+  const [state, dispatch] = React.useReducer(exampleReducer, initialState);
+  const {loading, results, value} = state;
+
+  const timeoutRef = React.useRef();
+  const handleSearchChange = React.useCallback((e, data) => {
+    clearTimeout(timeoutRef.current);
+    dispatch({type: "START_SEARCH", query: data.value});
+
+    timeoutRef.current = setTimeout(() => {
+      if (data.value.length === 0) {
+        dispatch({type: "CLEAN_QUERY"});
+        return;
+      }
+
+      const re = new RegExp(_.escapeRegExp(data.value), "i");
+      const isMatch = (result) => re.test(result.title);
+
+      dispatch({
+        type: "FINISH_SEARCH",
+        results: _.filter(source, isMatch),
+      });
+    }, 300);
+  }, []);
+  React.useEffect(() => {
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  }, []);
+
+  return (
+    <Search
+      loading={loading}
+      onResultSelect={(e, data) =>
+        dispatch({type: "UPDATE_SELECTION", selection: data.result.title})
+      }
+      onSearchChange={handleSearchChange}
+      results={results}
+      value={value}
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid columns={2} className={"search-row"}>
+      <span> Compare Air Quality in Two Cities in the USA</span>
+      <Grid.Row>
+        <Grid.Column>
+          <SearchExampleStandard></SearchExampleStandard>
+        </Grid.Column>
+        <Grid.Column>
+          <SearchExampleStandard></SearchExampleStandard>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 }
 
